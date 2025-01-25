2f9cd7dde0a5353449c5108aa3d053e55fce28b8
HailyV <hailyvuong910@gmail.com>
2025-01-24 22:11:12 -0800

    lab3

--- global.js
+++ global.js
@@ -27,21 +27,6 @@ document.body.insertAdjacentHTML(
 `
 );
 
-
-document.body.insertAdjacentHTML(
-  'afterbegin',
-  `
-  <label class="color-scheme">
-    Theme:
-    <select id="theme-switcher">
-      <option value="auto" selected>Automatic</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-    </select>
-  </label>
-`
-);
-
 // Function to initialize the theme on page load
 function initializeTheme() {
   const savedTheme = localStorage.getItem('theme') || 'auto';
